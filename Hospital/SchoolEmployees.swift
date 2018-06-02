d6e4aab45ffe24bb9a69ba2f35c0b9509668d072
Tor Magnus Michaelsen <tormagnusmichaelsen@Tors-MacBook-Pro.local>
2018-06-02 18:56:03 +0200

    finished, all tests passed

--- Hospital/SchoolEmployees.swift
+++ Hospital/SchoolEmployees.swift
@@ -3,9 +3,35 @@
 //  Hospital
 
 
+struct AdvisoryBoardMember {
+    var name: String
+    var vacationDays: Int = 30
+    
+    func wages() -> Double {
+        return 100_000.00
+    }
+}
 
+extension AdvisoryBoardMember: Employee{}
+//adopt protocol in extension: extension name: protocols to adopt{}
 
+struct Principal {
+    var name: String
+    var vacationDays: Int = 20
+    
+    func wages() -> Double {
+        return 80_000.00
+    }
+}
 
+extension Principal: Employee, Reprimand{}
+
+struct Teacher {
+    var name: String
+    var vacationDays: Int = 15
+}
+
+extension Teacher: Employee, Teach{}
 
 
 
